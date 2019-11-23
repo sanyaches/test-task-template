8c43f29a1cfedfad94840478c1e6363e7e822325
sanya_ches <sanyaches95@gmail.com>
2019-11-23 12:28:00 +0500

    first commit

---
+++ src/components/Contacts.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="contacts">
+    <h2>Contact me:</h2>
+    <p><b>Telegram:</b> sanya_ches</p>
+    <p><b>Gmail:</b> sanyaches95@gmail.com</p>
+    <p><b>Telephone number:</b> +7 (922) 620-70-05</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Contacts"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/MyContacts.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>This is an contact page</h1>
+    <contacts/>
+  </div>
+</template>
+<script>
+  import Contacts from "../components/Contacts";
+  export default {
+    components: {Contacts}
+  }
+</script>
